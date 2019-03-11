e630fdd0c262b9735d332ff225f107eba08f293f
thd7055 <thd7055@naver.com>
2019-03-11 22:34:15 +0900

    init commit
    ex01 numberString

---
+++ Ex01_numberString.java
@@ -0,0 +1,18 @@
+
+public class Ex01_numberString {
+
+	public static void main(String[] args) {
+		
+		// 자바에서는 따옴표가 없는 숫자는 숫자로 인식한다.
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		
+		// 곱하기를 할 때는 *(에스터리스크, Asterisk, 키보드 자판상으로 숫자 8 위)를 사용한다.
+		System.out.println(2*5);
+		
+		// 나누기를 할 때는 /(슬래쉬, slash, 키보드 자판상으로 오른쪽 shift 키 왼쪽)를 사용한다.
+		System.out.println(6/2);
+
+	}
+
+}
